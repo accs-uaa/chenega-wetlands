b189f3719a3a1bd4e06cd2480932ed71939d2617
Timm Nawrocki <twnawrocki@alaska.edu>
2021-12-09 09:35:37 -0900

    Add imagery pre-processing
    Added scripts to pre-process imagery for segmentation.

---
+++ package_GeospatialProcessing/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------------
+# Initialization for Geospatial Processing Module
+# Author: Timm Nawrocki
+# Last Updated: 2021-12-08
+# Usage: Individual functions have varying requirements. All functions that use arcpy must be executed in an ArcGIS Pro Python 3.6 distribution.
+# Description: This initialization file imports modules in the package so that the contents are accessible.
+# ---------------------------------------------------------------------------
+
+# Import functions from modules
+from package_GeospatialProcessing.arcpyGeoprocessing import arcpy_geoprocessing
+from package_GeospatialProcessing.compositeSegmentationImagery import composite_segmentation_imagery
+
