2867f52ecb2ed74fd9cd71107859e595ae7dc263
Timm Nawrocki <twnawrocki@alaska.edu>
2022-05-05 09:58:40 -0800

    Revert "Update scripts from Alphabet Hills"
    This reverts commit 583179b0efd936b8026abeac81c49ad71300e7e9.

--- geospatial_AnalyticGrids/GridIndex_CrossValidation.py
+++ geospatial_AnalyticGrids/GridIndex_CrossValidation.py
@@ -2,7 +2,7 @@
 # ---------------------------------------------------------------------------
 # Create cross-validation grid
 # Author: Timm Nawrocki
-# Last Updated: 2022-03-26
+# Last Updated: 2022-01-02
 # Usage: Must be executed in an ArcGIS Pro Python 3.7 installation.
 # Description: "Create cross-validation grid" creates a validation grid index from a manually-generated study area polygon.
 # ---------------------------------------------------------------------------
@@ -10,7 +10,6 @@
 # Import packages
 import arcpy
 from package_GeospatialProcessing import arcpy_geoprocessing
-from package_GeospatialProcessing import parse_image_segments
 from package_GeospatialProcessing import create_grid_index
 from package_GeospatialProcessing import convert_validation_grid
 import os
@@ -20,30 +19,26 @@ drive = 'N:/'
 root_folder = os.path.join(drive, 'ACCS_Work')
 
 # Define folder structure
-project_folder = os.path.join(drive, root_folder, 'Projects/WildlifeEcology/Moose_AlphabetHills/Data')
+project_folder = os.path.join(drive, root_folder, 'Projects/VegetationEcology/EPA_Chenega/Data')
 
 # Define geodatabases
-work_geodatabase = os.path.join(project_folder, 'AlphabetHillsBrowseBiomass.gdb')
-segments_geodatabase = os.path.join(project_folder, 'AlphabetHills_Segments.gdb')
+work_geodatabase = os.path.join(project_folder, 'EPA_Chenega.gdb')
 
 # Define input datasets
-alphabet_feature = os.path.join(work_geodatabase, 'Alphabet_StudyArea')
-alphabet_raster = os.path.join(project_folder, 'Data_Input/Alphabet_StudyArea.tif')
-segments_point = os.path.join(work_geodatabase, 'Alphabet_Segments_Final_Point')
-segments_polygon = os.path.join(work_geodatabase, 'Alphabet_Segments_Final_Polygon')
+chenega_feature = os.path.join(work_geodatabase, 'Chenega_ModelArea')
+chenega_raster = os.path.join(project_folder, 'Data_Input/Chenega_ModelArea.tif')
 
 # Define output grid datasets
-validation_grid = os.path.join(work_geodatabase, 'Alphabet_GridIndex_Validation_10km')
-validation_raster = os.path.join(project_folder, 'Data_Input/validation/Alphabet_ValidationGroups.tif')
-grid_folder = os.path.join(project_folder, 'Data_Input/imagery/segments/gridded')
+validation_grid = os.path.join(work_geodatabase, 'Chenega_GridIndex_Validation_5km')
+validation_raster = os.path.join(project_folder, 'Data_Input/validation/Chenega_ValidationGroups.tif')
 
 #### GENERATE VALIDATION GRID INDEX
 
 # Create key word arguments for the validation grid index
-validation_kwargs = {'distance': '10 Kilometers',
+validation_kwargs = {'distance': '5 Kilometers',
                      'grid_field': 'grid_validation',
                      'work_geodatabase': work_geodatabase,
-                     'input_array': [alphabet_feature],
+                     'input_array': [chenega_feature],
                      'output_array': [validation_grid]
                      }
 
@@ -60,11 +55,11 @@ else:
 
 # Create key word arguments for validation raster
 raster_kwargs = {'work_geodatabase': work_geodatabase,
-                 'input_array': [validation_grid, alphabet_feature, alphabet_raster],
+                 'input_array': [validation_grid, chenega_feature, chenega_raster],
                  'output_array': [validation_raster]
                  }
 
-# Generate validation group raster
+# Generate validation group raster for North American Beringia
 if arcpy.Exists(validation_raster) == 0:
     print('Converting validation grids to raster for North American Beringia...')
     arcpy_geoprocessing(convert_validation_grid, **raster_kwargs)
@@ -72,15 +67,3 @@ if arcpy.Exists(validation_raster) == 0:
 else:
     print('Validation raster already exists.')
     print('----------')
-
-#### PARSE REFINED IMAGE SEGMENTS FOR VALIDATION GRIDS
-
-parse_kwargs = {'tile_name': 'grid_validation',
-                'work_geodatabase': segments_geodatabase,
-                'input_array': [alphabet_raster, validation_grid, segments_point, segments_polygon],
-                'output_folder': grid_folder
-                }
-
-# Create buffered tiles for the major grid
-arcpy_geoprocessing(parse_image_segments, check_output=False, **parse_kwargs)
-print('----------')
--- statistics_Wetlands/02_Wetlands_Predict.py
+++ statistics_Wetlands/02_Wetlands_Predict.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 # ---------------------------------------------------------------------------
-# Predict physiograpy types to points
+# Predict wetland types to points
 # Author: Timm Nawrocki
-# Last Updated: 2022-03-27
+# Last Updated: 2022-01-05
 # Usage: Must be executed in an Anaconda Python 3.9+ distribution.
-# Description: "Predict physiography types to points" predicts a random forest model to a set of grid csv files containing extracted covariate values to produce a set of output predictions with mean and standard deviation. The script must be run on a machine that can support 4 cores.
+# Description: "Predict wetland types to points" predicts a random forest model to a set of grid csv files containing extracted covariate values to produce a set of output predictions with mean and standard deviation. The script must be run on a machine that can support 4 cores.
 # ---------------------------------------------------------------------------
 
 # Import packages
-import glob
 import joblib
 import os
 import pandas as pd
@@ -19,10 +18,7 @@ import datetime
 from package_Statistics import multiclass_predict
 
 # Define round
-round_date = 'round_20220331'
-
-# Define number of predicted classes
-class_number = 8
+round_date = 'round_20220105'
 
 #### SET UP DIRECTORIES, FILES, AND FIELDS
 
@@ -33,25 +29,26 @@ root_folder = 'ACCS_Work'
 # Define folder structure
 data_folder = os.path.join(drive,
                            root_folder,
-                           'Projects/WildlifeEcology/Moose_AlphabetHills/Data')
-input_folder = os.path.join(data_folder, 'Data_Input/training/table')
+                           'Projects/VegetationEcology/EPA_Chenega/Data')
+data_input = os.path.join(data_folder, 'Data_Input/training_data')
 model_folder = os.path.join(data_folder, 'Data_Output/model_results', round_date)
-output_folder = os.path.join(data_folder, 'Data_Output/predicted_tables', round_date)
+data_output = os.path.join(data_folder, 'Data_Output/predicted_tables', round_date)
 
 # Define input files
-os.chdir(input_folder)
-input_files = glob.glob('*.csv')
+input_file = os.path.join(data_input, 'AllPoints_ExtractedCovariates.csv')
 classifier_path = os.path.join(model_folder, 'classifier.joblib')
 
+# Define output files
+output_file = os.path.join(data_output, 'predicted_points.csv')
+
 # Define variable sets
-class_variable = ['train_class']
+class_variable = ['class_value']
 predictor_all = ['aspect', 'elevation', 'exposure', 'heat_load', 'position', 'radiation', 'roughness', 'slope',
                  'surface_area', 'surface_relief', 'wetness',
-                 'river_position', 'stream_position',
-                 'comp_01_blue', 'comp_02_green', 'comp_03_red', 'comp_04_nearir', 'comp_evi2', 'comp_ndvi', 'comp_ndwi',
-                 'comp_01_blue_std', 'comp_02_green_std', 'comp_03_red_std', 'comp_04_nearir_std',
-                 'comp_evi2_std', 'comp_ndvi_std', 'comp_ndwi_std',
-                 'vh', 'vv', 'burn_diff',
+                 'maxr_01_blue', 'maxr_02_green', 'maxr_03_red', 'maxr_04_nearir', 'maxr_evi2', 'maxr_ndvi', 'maxr_ndwi',
+                 'maxr_01_blue_std', 'maxr_02_green_std', 'maxr_03_red_std', 'maxr_04_nearir_std',
+                 'maxr_evi2_std', 'maxr_ndvi_std', 'maxr_ndwi_std',
+                 'vh', 'vv',
                  's2_06_02_blue', 's2_06_03_green', 's2_06_04_red', 's2_06_05_rededge1', 's2_06_06_rededge2',
                  's2_06_07_rededge3', 's2_06_08_nearir', 's2_06_08a_rededge4', 's2_06_11_shortir1', 's2_06_12_shortir2',
                  's2_06_evi2', 's2_06_nbr', 's2_06_ndmi', 's2_06_ndsi', 's2_06_ndvi', 's2_06_ndwi',
@@ -64,12 +61,9 @@ predictor_all = ['aspect', 'elevation', 'exposure', 'heat_load', 'position', 'ra
                  's2_09_02_blue', 's2_09_03_green', 's2_09_04_red', 's2_09_05_rededge1', 's2_09_06_rededge2',
                  's2_09_07_rededge3', 's2_09_08_nearir', 's2_09_08a_rededge4', 's2_09_11_shortir1', 's2_09_12_shortir2',
                  's2_09_evi2', 's2_09_nbr', 's2_09_ndmi', 's2_09_ndsi', 's2_09_ndvi', 's2_09_ndwi']
-retain_variables = ['segment_id', 'POINT_X', 'POINT_Y',
-                    'fol_alnus', 'fol_betshr', 'fol_bettre', 'fol_dectre', 'fol_dryas', 'fol_empnig', 'fol_erivag',
-                    'fol_picgla', 'fol_picmar', 'fol_rhoshr', 'fol_salshr', 'fol_sphagn', 'fol_vaculi', 'fol_vacvit',
-                    'fol_wetsed']
-prediction = ['physiography']
-output_columns = retain_variables + predictor_all
+retain_variables = ['gridcode', 'POINT_X', 'POINT_Y']
+prediction = ['prediction']
+output_columns = retain_variables + class_variable + prediction
 
 # Define random state
 rstate = 21
@@ -85,60 +79,41 @@ segment_success_time = datetime.datetime.now()
 print(f'Completed at {segment_success_time.strftime("%Y-%m-%d %H:%M")} (Elapsed time: {datetime.timedelta(seconds=segment_elapsed)})')
 print('----------')
 
-# Predict each input dataset
-count = 1
-input_length = len(input_files)
-for file in input_files:
-    # Define output file
-    output_file = os.path.join(output_folder, os.path.split(file)[1])
+# Load input data
+print('Loading input data')
+segment_start = time.time()
+all_data = pd.read_csv(input_file)
+input_data = all_data[retain_variables + class_variable + predictor_all].copy()
+input_data = input_data.dropna(axis=0, how='any')
+print(len(input_data))
+X_data = input_data[predictor_all].astype(float)
+# Prepare output_data
+output_data = input_data[retain_variables + class_variable]
+# Report success
+segment_end = time.time()
+segment_elapsed = int(segment_end - segment_start)
+segment_success_time = datetime.datetime.now()
+print(f'Completed at {segment_success_time.strftime("%Y-%m-%d %H:%M")} (Elapsed time: {datetime.timedelta(seconds=segment_elapsed)})')
+print('----------')
 
-    # Predict input dataset if output does not already exist
-    if os.path.exists(output_file) == 0:
-        print(f'Predicting input dataset {count} out of {input_length}...')
+# Predict data
+print('Predicting classes to points...')
+segment_start = time.time()
+output_data = multiclass_predict(classifier, X_data, output_data)
+# Report success
+segment_end = time.time()
+segment_elapsed = int(segment_end - segment_start)
+segment_success_time = datetime.datetime.now()
+print(f'Completed at {segment_success_time.strftime("%Y-%m-%d %H:%M")} (Elapsed time: {datetime.timedelta(seconds=segment_elapsed)})')
+print('----------')
 
-        # Load input data
-        print('\tLoading input data')
-        segment_start = time.time()
-        all_data = pd.read_csv(file)
-        input_data = all_data[retain_variables + class_variable + predictor_all].copy()
-        input_data = input_data.dropna(axis=0, how='any')
-        print(f'\tInput dataset contains {len(input_data)} rows...')
-        X_data = input_data[predictor_all].astype(float)
-        # Prepare output_data
-        output_data = input_data[output_columns]
-        # Report success
-        segment_end = time.time()
-        segment_elapsed = int(segment_end - segment_start)
-        segment_success_time = datetime.datetime.now()
-        print(f'\tCompleted at {segment_success_time.strftime("%Y-%m-%d %H:%M")} (Elapsed time: {datetime.timedelta(seconds=segment_elapsed)})')
-        print('\t----------')
-
-        # Predict data
-        print('\tPredicting classes to points...')
-        segment_start = time.time()
-        output_data = multiclass_predict(classifier, X_data, prediction, class_number, output_data)
-        # Report success
-        segment_end = time.time()
-        segment_elapsed = int(segment_end - segment_start)
-        segment_success_time = datetime.datetime.now()
-        print(f'\tCompleted at {segment_success_time.strftime("%Y-%m-%d %H:%M")} (Elapsed time: {datetime.timedelta(seconds=segment_elapsed)})')
-        print('\t----------')
-
-        # Export output data to csv
-        print('\tExporting predictions to csv...')
-        segment_start = time.time()
-        output_data.to_csv(output_file, header=True, index=False, sep=',', encoding='utf-8')
-        # Report success
-        segment_end = time.time()
-        segment_elapsed = int(segment_end - segment_start)
-        segment_success_time = datetime.datetime.now()
-        print(f'\tCompleted at {segment_success_time.strftime("%Y-%m-%d %H:%M")} (Elapsed time: {datetime.timedelta(seconds=segment_elapsed)})')
-        print('\t----------')
-
-    else:
-        # Return message that output already exists
-        print(f'Output dataset {count} out of {input_length} already exists.')
-
-    # Increase count
-    count += 1
-    print('----------')
+# Export output data to csv
+print('Exporting predictions to csv...')
+segment_start = time.time()
+output_data.to_csv(output_file, header=True, index=False, sep=',', encoding='utf-8')
+# Report success
+segment_end = time.time()
+segment_elapsed = int(segment_end - segment_start)
+segment_success_time = datetime.datetime.now()
+print(f'Completed at {segment_success_time.strftime("%Y-%m-%d %H:%M")} (Elapsed time: {datetime.timedelta(seconds=segment_elapsed)})')
+print('----------')
