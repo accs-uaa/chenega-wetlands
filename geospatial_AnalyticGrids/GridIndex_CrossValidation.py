583179b0efd936b8026abeac81c49ad71300e7e9
Timm Nawrocki <twnawrocki@alaska.edu>
2022-05-05 07:33:38 -0800

    Update scripts from Alphabet Hills
    Update scripts from Alphabet Hills project.

--- geospatial_AnalyticGrids/GridIndex_CrossValidation.py
+++ geospatial_AnalyticGrids/GridIndex_CrossValidation.py
@@ -2,7 +2,7 @@
 # ---------------------------------------------------------------------------
 # Create cross-validation grid
 # Author: Timm Nawrocki
-# Last Updated: 2022-01-02
+# Last Updated: 2022-03-26
 # Usage: Must be executed in an ArcGIS Pro Python 3.7 installation.
 # Description: "Create cross-validation grid" creates a validation grid index from a manually-generated study area polygon.
 # ---------------------------------------------------------------------------
@@ -10,6 +10,7 @@
 # Import packages
 import arcpy
 from package_GeospatialProcessing import arcpy_geoprocessing
+from package_GeospatialProcessing import parse_image_segments
 from package_GeospatialProcessing import create_grid_index
 from package_GeospatialProcessing import convert_validation_grid
 import os
@@ -19,26 +20,30 @@ drive = 'N:/'
 root_folder = os.path.join(drive, 'ACCS_Work')
 
 # Define folder structure
-project_folder = os.path.join(drive, root_folder, 'Projects/VegetationEcology/EPA_Chenega/Data')
+project_folder = os.path.join(drive, root_folder, 'Projects/WildlifeEcology/Moose_AlphabetHills/Data')
 
 # Define geodatabases
-work_geodatabase = os.path.join(project_folder, 'EPA_Chenega.gdb')
+work_geodatabase = os.path.join(project_folder, 'AlphabetHillsBrowseBiomass.gdb')
+segments_geodatabase = os.path.join(project_folder, 'AlphabetHills_Segments.gdb')
 
 # Define input datasets
-chenega_feature = os.path.join(work_geodatabase, 'Chenega_ModelArea')
-chenega_raster = os.path.join(project_folder, 'Data_Input/Chenega_ModelArea.tif')
+alphabet_feature = os.path.join(work_geodatabase, 'Alphabet_StudyArea')
+alphabet_raster = os.path.join(project_folder, 'Data_Input/Alphabet_StudyArea.tif')
+segments_point = os.path.join(work_geodatabase, 'Alphabet_Segments_Final_Point')
+segments_polygon = os.path.join(work_geodatabase, 'Alphabet_Segments_Final_Polygon')
 
 # Define output grid datasets
-validation_grid = os.path.join(work_geodatabase, 'Chenega_GridIndex_Validation_5km')
-validation_raster = os.path.join(project_folder, 'Data_Input/validation/Chenega_ValidationGroups.tif')
+validation_grid = os.path.join(work_geodatabase, 'Alphabet_GridIndex_Validation_10km')
+validation_raster = os.path.join(project_folder, 'Data_Input/validation/Alphabet_ValidationGroups.tif')
+grid_folder = os.path.join(project_folder, 'Data_Input/imagery/segments/gridded')
 
 #### GENERATE VALIDATION GRID INDEX
 
 # Create key word arguments for the validation grid index
-validation_kwargs = {'distance': '5 Kilometers',
+validation_kwargs = {'distance': '10 Kilometers',
                      'grid_field': 'grid_validation',
                      'work_geodatabase': work_geodatabase,
-                     'input_array': [chenega_feature],
+                     'input_array': [alphabet_feature],
                      'output_array': [validation_grid]
                      }
 
@@ -55,11 +60,11 @@ else:
 
 # Create key word arguments for validation raster
 raster_kwargs = {'work_geodatabase': work_geodatabase,
-                 'input_array': [validation_grid, chenega_feature, chenega_raster],
+                 'input_array': [validation_grid, alphabet_feature, alphabet_raster],
                  'output_array': [validation_raster]
                  }
 
-# Generate validation group raster for North American Beringia
+# Generate validation group raster
 if arcpy.Exists(validation_raster) == 0:
     print('Converting validation grids to raster for North American Beringia...')
     arcpy_geoprocessing(convert_validation_grid, **raster_kwargs)
@@ -67,3 +72,15 @@ if arcpy.Exists(validation_raster) == 0:
 else:
     print('Validation raster already exists.')
     print('----------')
+
+#### PARSE REFINED IMAGE SEGMENTS FOR VALIDATION GRIDS
+
+parse_kwargs = {'tile_name': 'grid_validation',
+                'work_geodatabase': segments_geodatabase,
+                'input_array': [alphabet_raster, validation_grid, segments_point, segments_polygon],
+                'output_folder': grid_folder
+                }
+
+# Create buffered tiles for the major grid
+arcpy_geoprocessing(parse_image_segments, check_output=False, **parse_kwargs)
+print('----------')
---
+++ postprocess_Wetlands/01_Raster_Wetlands.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------------
+# Create physiography rasters
+# Author: Timm Nawrocki
+# Last Updated: 2022-03-27
+# Usage: Must be executed in an ArcGIS Pro Python 3.7 installation.
+# Description: "Create physiography raster" combines tiles into a physiography raster.
+# ---------------------------------------------------------------------------
+
+# Import packages
+import os
+from package_GeospatialProcessing import arcpy_geoprocessing
+from package_GeospatialProcessing import predictions_to_raster
+
+# Set round date
+round_date = 'round_20220331'
+
+# Set root directory
+drive = 'N:/'
+root_folder = 'ACCS_Work'
+
+# Define folder structure
+project_folder = os.path.join(drive, root_folder, 'Projects/WildlifeEcology/Moose_AlphabetHills/Data')
+segment_folder = os.path.join(project_folder, 'Data_Input/imagery/segments/gridded')
+prediction_folder = os.path.join(project_folder, 'Data_Output/predicted_tables', round_date)
+grid_folder = os.path.join(project_folder, 'Data_Output/predicted_rasters', round_date, 'physiography')
+output_folder = os.path.join(project_folder, 'Data_Output/output_rasters', round_date)
+
+# Define geodatabases
+work_geodatabase = os.path.join(project_folder, 'AlphabetHillsBrowseBiomass.gdb')
+
+# Define input datasets
+alphabet_raster = os.path.join(project_folder, 'Data_Input/Alphabet_StudyArea.tif')
+
+# Define output raster
+output_raster = os.path.join(output_folder, 'Alphabet_Physiography.tif')
+
+# Define physiography dictionary
+physiography_dictionary = {'barren': 1,
+                           'burned': 2,
+                           'drainage': 3,
+                           'riparian': 4,
+                           'floodplain': 5,
+                           'water': 6,
+                           'upland/lowland': 7,
+                           'aspen': 8
+                           }
+
+# Create key word arguments
+kwargs_attributes = {'segment_folder': segment_folder,
+                     'prediction_folder': prediction_folder,
+                     'grid_folder': grid_folder,
+                     'target_field': 'physiography',
+                     'attribute_dictionary': physiography_dictionary,
+                     'work_geodatabase': work_geodatabase,
+                     'input_array': [alphabet_raster],
+                     'output_array': [output_raster]
+                     }
+
+# Convert predictions to physiography raster
+print(f'Converting predictions to physiography raster...')
+arcpy_geoprocessing(predictions_to_raster, **kwargs_attributes)
+print('----------')
